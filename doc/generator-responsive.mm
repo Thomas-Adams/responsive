3ec968d10a7ecc380686273fcefabd0ba59758fb
heinrich.adams@gmail.com <heinrich.adams@gmail.com>
2016-01-06 17:39:04 +0800

    added diagram

---
+++ doc/generator-responsive.mm
@@ -0,0 +1,422 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<attribute_registry FONT_SIZE="12">
+    <attribute_name MANUAL="true" NAME="JavaScript">
+        <attribute_value VALUE=""/>
+        <attribute_value VALUE="html5shiv"/>
+        <attribute_value VALUE="modernizr"/>
+        <attribute_value VALUE="respond.js"/>
+        <attribute_value VALUE="selectivizr.js"/>
+    </attribute_name>
+</attribute_registry>
+<node LOCALIZED_TEXT="new_mindmap" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1450922497615"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true" show_notes_in_map="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="11"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Devices" POSITION="right" ID="ID_1006581799" CREATED="1450695943757" MODIFIED="1450695960579">
+<edge COLOR="#ff0000"/>
+<node TEXT="Mobile" ID="ID_86220738" CREATED="1450695963819" MODIFIED="1450695987731"/>
+<node TEXT="Responsive" ID="ID_432713047" CREATED="1450695972740" MODIFIED="1450695978080">
+<node TEXT="Support IE8" ID="ID_432606778" CREATED="1450696692378" MODIFIED="1450925131893">
+<attribute_layout NAME_WIDTH="132"/>
+<attribute NAME="JavaScript" VALUE="selectivizr.js"/>
+<attribute NAME="JavaScript" VALUE="respond.js"/>
+<attribute NAME="JavaScript" VALUE="html5shiv"/>
+</node>
+<node TEXT="No IE 8" ID="ID_1639209377" CREATED="1450696706137" MODIFIED="1450696710910"/>
+</node>
+</node>
+<node TEXT="Page navigation and loading" POSITION="left" ID="ID_1423605986" CREATED="1450695994072" MODIFIED="1450696015914">
+<edge COLOR="#0000ff"/>
+<node TEXT="AJAX based preloading of single pages" ID="ID_1002973079" CREATED="1450696018365" MODIFIED="1450696032666"/>
+<node TEXT="Endless scrolling with AJAX based preloading" ID="ID_1443673557" CREATED="1450696035326" MODIFIED="1450696059208"/>
+<node TEXT="Endless scrolling no preloading" ID="ID_1674432588" CREATED="1450696062549" MODIFIED="1450696083412"/>
+</node>
+<node TEXT="Navigation styles" POSITION="right" ID="ID_1864041085" CREATED="1450696097124" MODIFIED="1450696103134">
+<edge COLOR="#00ff00"/>
+<node TEXT="Off-Canvas navigation" ID="ID_146113134" CREATED="1450696104654" MODIFIED="1450696115485">
+<node TEXT="Slide navigation" ID="ID_868438263" CREATED="1450854341186" MODIFIED="1450854352837">
+<node TEXT="simple" ID="ID_170549519" CREATED="1450854363728" MODIFIED="1451969119041">
+<attribute NAME="URL example" VALUE="http://localhost:3001/slidebars-menu.html" OBJECT="java.net.URI|http://localhost:3001/slidebars-menu.html"/>
+<attribute NAME="From" VALUE="https://www.youtube.com/watch?v=d4P8s-mkMvs&amp;list=PLB1tHjY6_AzltKYY1LzY7U2ADRNSV9hDL" OBJECT="java.net.URI|https://www.youtube.com/watch?v=d4P8s-mkMvs&amp;list=PLB1tHjY6_AzltKYY1LzY7U2ADRNSV9hDL"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Font subsetting" POSITION="left" ID="ID_1580484997" CREATED="1450696156850" MODIFIED="1450696162021">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Fonts" ID="ID_461648845" CREATED="1450696600488" MODIFIED="1450696605580"/>
+</node>
+<node TEXT="SASS framework" POSITION="right" ID="ID_836088528" CREATED="1450696170341" MODIFIED="1450696178462">
+<edge COLOR="#00ffff"/>
+<node TEXT="Framework libraries" ID="ID_1457095607" CREATED="1450696180542" MODIFIED="1450696187933">
+<node TEXT="Compass" ID="ID_1207065309" CREATED="1450696905042" MODIFIED="1450696908403"/>
+<node TEXT="Bitter" ID="ID_1358159860" CREATED="1450696910833" MODIFIED="1450696916974"/>
+<node TEXT="Neat" ID="ID_250835818" CREATED="1450696919074" MODIFIED="1450696925395"/>
+<node TEXT="Bourbon" ID="ID_436047911" CREATED="1450696927615" MODIFIED="1450696945448"/>
+<node TEXT="Refills" ID="ID_1892334197" CREATED="1450696949599" MODIFIED="1450696984545"/>
+<node TEXT="Bootstrap" ID="ID_1745468453" CREATED="1450696987476" MODIFIED="1450696990856"/>
+</node>
+</node>
+<node TEXT="Locale support" POSITION="left" ID="ID_648817216" CREATED="1450696198045" MODIFIED="1450696228668">
+<edge COLOR="#ffff00"/>
+<node TEXT="locales" ID="ID_605976017" CREATED="1450696206205" MODIFIED="1450696243050">
+<node TEXT="CN" ID="ID_1285035189" CREATED="1450696244910" MODIFIED="1450696247730">
+<node TEXT="zh" ID="ID_1171656851" CREATED="1450696248890" MODIFIED="1450696251580"/>
+<node TEXT="en" ID="ID_179789177" CREATED="1450696254427" MODIFIED="1450696259047"/>
+</node>
+</node>
+<node TEXT="no locales" ID="ID_579451953" CREATED="1450696216076" MODIFIED="1450696236179"/>
+</node>
+<node TEXT="Animations" POSITION="right" ID="ID_1076892035" CREATED="1450696277749" MODIFIED="1450696281379">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Basis" POSITION="left" ID="ID_1182572562" CREATED="1450696732415" MODIFIED="1450696738525">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Dynamic feature detection" POSITION="right" ID="ID_235351436" CREATED="1450697347536" MODIFIED="1450697358597">
+<edge COLOR="#007c00"/>
+<node TEXT="Yes" ID="ID_1360175413" CREATED="1450697366629" MODIFIED="1450697391673">
+<attribute NAME="JavaScript" VALUE="modernizr"/>
+</node>
+<node TEXT="No" ID="ID_1454408427" CREATED="1450697373451" MODIFIED="1450697375221"/>
+</node>
+<node TEXT="Requirements fro mobile" POSITION="left" ID="ID_998621837" CREATED="1450916171221" MODIFIED="1450930796167">
+<edge COLOR="#7c007c"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      for details see here : <a href=" http://mobile-frameworks-comparison-chart.com/">mobile frameworks comparison charts</a>
+    </p>
+    <p>
+      Based on given requirements the possible choices are:
+    </p>
+    <table>
+      <tr>
+        <th>
+          Framework
+        </th>
+        <th>
+          Link
+        </th>
+        <th>
+          Remarks
+        </th>
+        <th>
+          Suitable?
+        </th>
+      </tr>
+      <tr>
+        <td>
+          ChocolateChip-UI
+        </td>
+        <td>
+          <a href="http://www.chocolatechip-ui.com/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              based on JQuery
+            </li>
+            <li>
+              Node.js support
+            </li>
+            <li>
+              LESS support, no SASS
+            </li>
+            <li>
+              Support of themes
+            </li>
+            <li>
+              only a few UI components
+            </li>
+          </ul>
+        </td>
+        <td background="red">
+          Maybe
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Dojo Mobile
+        </td>
+        <td>
+          <a href="http://dojotoolkit.org/features/mobile">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Needs Dojo, different than JQuery
+            </li>
+          </ul>
+        </td>
+        <td style="text-align: left">
+          NO
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Framework 7
+        </td>
+        <td>
+          <a href="http://idangero.us/framework7/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Godd candidate
+            </li>
+          </ul>
+        </td>
+        <td>
+          Good candidate Although has focus on iOS but claims also be fit for Android
+        </td>
+      </tr>
+      <tr>
+        <td>
+          JQ Touch
+        </td>
+        <td>
+          <a href="http://jqtouch.com/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Outdated, no documentation that could be of use
+            </li>
+          </ul>
+        </td>
+        <td>
+          NO
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Kendo UI
+        </td>
+        <td>
+          <a href="http://www.kendoui.com/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Not opensource anymore, nor free
+            </li>
+          </ul>
+        </td>
+        <td>
+          NO
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Mobify.js
+        </td>
+        <td>
+          <a href="http://www.mobifyjs.com//">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Very few features, capturing and images, not really a framework
+            </li>
+          </ul>
+        </td>
+        <td>
+          NO
+        </td>
+      </tr>
+      <tr>
+        <td>
+          The M-Project
+        </td>
+        <td>
+          <a href="http://www.the-m-project.org/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Still beta with a few features only
+            </li>
+          </ul>
+        </td>
+        <td>
+          NO
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Onsen UI
+        </td>
+        <td>
+          <a href="http://www.onsen.io/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              More for appas than for website
+            </li>
+          </ul>
+        </td>
+        <td>
+          Maybe
+        </td>
+      </tr>
+      <tr>
+        <td>
+          qooxdoo
+        </td>
+        <td>
+          <a href="http://qooxdoo.org/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              More a replacement for JQuery
+            </li>
+          </ul>
+        </td>
+        <td>
+          Maybe
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Sencha Touch
+        </td>
+        <td>
+          <a href="https://www.sencha.com/products/touch/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Not opensource anymore, nor free
+            </li>
+          </ul>
+        </td>
+        <td>
+          NO
+        </td>
+      </tr>
+      <tr>
+        <td>
+          wink
+        </td>
+        <td>
+          <a href="http://www.winktoolkit.org/">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Lightweight API, but rather exotic, few features only
+            </li>
+          </ul>
+        </td>
+        <td>
+          Maybe
+        </td>
+      </tr>
+      <tr>
+        <td>
+          GMU.js
+        </td>
+        <td>
+          <a href="http://gmu.baidu.com/getstarted">Official Website</a>
+        </td>
+        <td>
+          <ul>
+            <li>
+              Lightweight API, zepto based Advantage of customized components Disadvantage 2 years old, no bugfixes?, vanilla components, no styling support
+            </li>
+          </ul>
+        </td>
+        <td>
+          Maybe/in the long we should look for something else
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="OS" ID="ID_269966552" CREATED="1450916177150" MODIFIED="1450916189643">
+<node TEXT="iOS" ID="ID_1235126604" CREATED="1450916271120" MODIFIED="1450916275685"/>
+<node TEXT="Android" ID="ID_433885351" CREATED="1450916277695" MODIFIED="1450916280355"/>
+</node>
+<node TEXT="Target" ID="ID_1034671187" CREATED="1450916196975" MODIFIED="1450916203894">
+<node TEXT="Mobile website" ID="ID_1950201008" CREATED="1450916283265" MODIFIED="1450916288296"/>
+</node>
+<node TEXT="Language" ID="ID_1911738166" CREATED="1450916209905" MODIFIED="1450916215065">
+<node TEXT="HTML" ID="ID_1498098402" CREATED="1450916290626" MODIFIED="1450916294406"/>
+<node TEXT="CSS" ID="ID_776578338" CREATED="1450916296136" MODIFIED="1450916298767"/>
+<node TEXT="Javascript" ID="ID_862588250" CREATED="1450916300367" MODIFIED="1450916305407"/>
+</node>
+<node TEXT="Hardware features" ID="ID_1988681400" CREATED="1450916218505" MODIFIED="1450916239888">
+<node TEXT="Gestures + Multitouch" ID="ID_452042357" CREATED="1450916315108" MODIFIED="1450916331871"/>
+</node>
+<node TEXT="UI" ID="ID_863432531" CREATED="1450916253539" MODIFIED="1450916255889">
+<node TEXT="Nice to have" ID="ID_1005567546" CREATED="1450916341702" MODIFIED="1450916345182"/>
+</node>
+<node TEXT="License" ID="ID_1584792046" CREATED="1450916262540" MODIFIED="1450916265530">
+<node TEXT="Open Source" ID="ID_1460808245" CREATED="1450916348833" MODIFIED="1450916355553"/>
+</node>
+<node TEXT="Broad community" ID="ID_1389432840" CREATED="1450916371816" MODIFIED="1450916379687"/>
+</node>
+<node TEXT="Requirements for responsiveness" POSITION="right" ID="ID_1290493871" CREATED="1450924651078" MODIFIED="1450924672780">
+<edge COLOR="#007c7c"/>
+</node>
+</node>
+</map>
